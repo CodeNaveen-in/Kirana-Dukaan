eac42fd2d0c5d78896fe3bb81a283a52a29b4ac2
Naveen Garg <nvngrg0102@gmail.com>
2026-01-06 23:03:29 +0530

    Milestone 3 :Form validation, Password Hashing and user updating to database

--- models.py
+++ models.py
@@ -8,6 +8,7 @@ class User(db.Model):
     username = db.Column(db.String(32), unique=True)
     passhash = db.Column(db.String(256), nullable=False)
     name = db.Column(db.String(64), nullable=True)
+    email = db.Column(db.String(120), unique=True, nullable=True)
     is_admin = db.Column(db.Boolean, nullable=False, default=False)
 
 class Category(db.Model):
--- routes.py
+++ routes.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template, url_for
+from flask import Blueprint, render_template, url_for, request, redirect, flash
+from models import db, User, Category
+from werkzeug.security import generate_password_hash, check_password_hash
 
 # Create a Blueprint object
 main = Blueprint('main', __name__)
@@ -18,5 +20,34 @@ def login():
 @main.route('/register')
 def register():
     return render_template('register.html')
+
+@main.route('/register', methods=["POST"])
+def register_post():
+    name = request.form.get('name')
+    username = request.form.get('username')
+    email = request.form.get('email')
+    password = request.form.get('password')
+    confirm_password = request.form.get('confirm_password')
+
+    if (not username or not email or not password or not confirm_password):
+        flash('Please fill in the important Fields')
+        return redirect(url_for('main.register'))
+    
+    if (password != confirm_password):
+        flash('Passwords do not match')
+        return redirect(url_for('main.register'))
+    
+    user = User.query.filter_by(username=username).first()
+
+    if user:
+        flash('Username already exists')
+        return redirect(url_for('main.register'))
+    
+    passhash = generate_password_hash(password)
+    new_user = User(username=username, passhash=passhash, name=name, email=email)
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect(url_for('main.login'))
+
 
 # Change your other @app.route to @main.route...
